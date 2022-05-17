4956ed761552635a415f132dd334e4f01ccd390f
lni <luc.nicolas@itesoft.com>
2022-05-17 09:31:33 +0200

    initial commit

---
+++ Notepad/Notepad/NoteEditPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Notepad
+{
+    public partial class NoteEditPage : ContentPage
+    {
+        private string noteName;
+
+        public NoteEditPage(string noteName)
+        {
+            this.noteName = noteName;
+
+            InitializeComponent();
+
+            Title = noteName;
+
+            // Rechargement de la note sauvegardée
+            string directory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string filePath = Path.Combine(directory, $"{noteName}.txt");
+            if (File.Exists(filePath))
+            {
+                noteEditor.Text = File.ReadAllText(filePath);
+            }
+        }
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            // Récupérer le contenu de l'éditeur
+            string content = noteEditor.Text;
+
+            // Sauvegarder le contenu dans un fichier
+            string directory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string filePath = Path.Combine(directory, $"{noteName}.txt");
+            File.WriteAllText(filePath, content);
+
+            // Retour à la liste
+            await Navigation.PopAsync();
+        }
+
+        private async void DeleteButton_Clicked(object sender, EventArgs e)
+        {
+            // Confirmation
+            bool deleteFile = await DisplayAlert("Suppression", "Voulez-vous vraiment supprimer cette note", "Oui", "Non");
+            if (!deleteFile)
+                return;
+
+            // Suppression du texte
+            noteEditor.Text = "";
+
+            // Suppression du fichier
+            string directory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string filePath = Path.Combine(directory, $"{noteName}.txt");
+            File.Delete(filePath);
+            
+            // Retour à la liste
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ Notepad/Notepad/NoteListPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notepad
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteListPage : ContentPage
+    {
+        public NoteListPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            string directory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string[] files = Directory.GetFiles(directory, "*.txt");
+
+            noteList.ItemsSource = files.Select(x => Path.GetFileNameWithoutExtension(x));
+        }
+
+        private async void noteList_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Page page = new NoteEditPage((string)e.Item);
+            await Navigation.PushAsync(page);
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            string noteName = await DisplayPromptAsync("Nouvelle note", "Nom pour la note:");
+            if (string.IsNullOrEmpty(noteName))
+                return;
+
+            // Création du fichier
+            string directory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string filePath = Path.Combine(directory, $"{noteName}.txt");
+            File.WriteAllText(filePath, "");
+
+            // Afficher la note créée
+            Page page = new NoteEditPage(noteName);
+            await Navigation.PushAsync(page);
+        }
+    }
+}
